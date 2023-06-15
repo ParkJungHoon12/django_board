8ac46693df1f626f8136fcc599b57698fe1603f8
ParkJungHoon12 <junghunpocat@naver.com>
2023-06-15 15:30:45 +0900

    finalboard

--- board_project/board_main/models.py
+++ board_project/board_main/models.py
@@ -13,7 +13,7 @@ class Test(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique = True)
     password = models.CharField(max_length=30)
     # DB설정에 default time stamp가 걸리는 것이 아닌, 장고가 현재시간을 db에 insert
     created_at = models.DateTimeField(auto_now_add=True) #만들어질때만 데이타가 입력
@@ -27,5 +27,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True) #변경사항이 나올떄만 데이타가 입력
     # DB에는 fk를 설정한 변수명에 id를 붙게 된다.  
     # on_update = models.CASCADE, on_delete = models.CASCADE,
-    author = models.ForeignKey(Author, on_delete = models.SET_NULL, null=True)
 
+    # DB의 author_id는 파이썬(장고)에서는 author객체와 같은 것이다.
+    author = models.ForeignKey(Author, on_delete = models.SET_NULL, null=True, related_name = 'posts')
+    # Post는 객체 생성시 Author객체를 자동 생성하기 때문에 코드에서 값을 참조 가능하지만
+    # Author는 객체 생성시 Post값을 참조하기 어렵다.(예로 작가 1명당 몇개의 글을 썼는지)
+    # 이럴떄는 위 문구처럼 author의 파라미터로 이름을 지정 related_name = 'posts'해서 외부에서 참조 가능함
+    # (장고에서 지원하는 기능임으로 이해하면 됨)
--- board_project/board_main/views.py
+++ board_project/board_main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Author, Post
+from django.http import HttpResponse, HttpResponseNotFound
 
 def home(request):
     return render(request, 'home.html')
+
 
 def author_list(request):
     authors = Author.objects.all()
@@ -33,11 +35,6 @@ def author_update(request, my_id):
         return render(request, 'author/author_update.html', {'author': author})
 
 
-
-
-def post_list(request):
-    return render(request, 'post/post_list.html')
-
 def author_new(request):
      
      if request.method == 'POST':  
@@ -56,3 +53,58 @@ def author_new(request):
      else: 
         return render(request, 'author/author_new.html')
 
+
+def post_list(request):
+    # filter주고 값을 안주면 all과 일치
+    # order_by 하고 -컬럼명 이렇게 주면 내림차순정렬
+    posts = Post.objects.filter().order_by('-created_at')
+    return render(request, 'post/post_list.html', {'posts': posts})
+
+
+def post_detail(request, my_id):
+    post = Post.objects.get(id = my_id)
+    return render(request, 'post/post_detail.html', {'post': post})
+
+
+def post_update(request, my_id):
+    post = Post.objects.get(id = my_id)
+    if request.method == 'POST':         
+        my_title = request.POST['my_title']
+        my_contents = request.POST['my_contents']
+
+        post.title = my_title 
+        post.contents = my_contents
+        post.save() 
+       
+        return redirect('/') 
+    
+    else: 
+        return render(request, 'post/post_update.html', {'post': post})
+    
+
+def post_new(request):    
+     if request.method == 'POST':  
+        my_title = request.POST['my_title']
+        my_contents = request.POST['my_contents']
+        my_email = request.POST['my_email'] # 이메일 추가
+
+        p1 = Post() 
+        if my_email:
+           try:
+               p1.author  = Author.objects.get(email = my_email) 
+           # {id:1. name"hong, email"aaa@aa ....} email = my_email인 db 행 1개의 전체값을 객체(p1.author)에 저장
+           # 장고에서는 id값만 빼서 post db에 알아서 자동 저장함
+    
+           except Author.DoesNotExist:
+               # HttpResponse : 200 정상
+               # HttpResponseNotFound : 404 에러(사용자 오류)
+               return HttpResponseNotFound("존재하지 않는 이메일입니다.")
+          
+        p1.title = my_title
+        p1.contents = my_contents
+        p1.save()
+
+        return redirect('/') 
+     
+     else: 
+        return render(request, 'post/post_new.html')
